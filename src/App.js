0229405c0a60c622b0ec90d7d2baa6aa89fbaf1d
menna elshahat <mennaelshahat88@gmail.com>
2022-08-25 20:10:59 +0200

    Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Navs from './components/Navs';
+import  ReactDOM from 'react-dom/client';
+import Home from './components/Home';
+import Products from './components/Products';
+import { BrowserRouter, Routes,Router,Route,Switch } from 'react-router-dom';
+import ProductDetails from './components/ProductDetails';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <BrowserRouter>
+       
+      <Navs />       
+        <Routes>
+          <Route exact path='/' component={<Home />} />
+          <Route exact  path="/Product" element = {<Products />} />       
+          <Route exact path="/cart" element = {<Cart />} /> 
+          <Route exact path="/checkout" element = {<Checkout />} /> 
+
+          <Route  path="/ProductDetails/:id" element = {<ProductDetails />} /> 
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter} from 'react-router-dom';
+import store from './Redux/store';
+import { Provider } from 'react-redux';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import Products from './components/Products';
+
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+  crossorigin="anonymous"
+/>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+<script
+  src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+  crossorigin></script>
 root.render(
   <React.StrictMode>
+    <Provider store={store} >
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
